3cd8649c20630aed7397907facbe0838cd558914
Gilbert <djbuzzkill@gmail.com>
2022-09-26 01:46:39 -0600

    no quit cmd

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -308,7 +308,7 @@ globalkeys = gears.table.join (
        -- Standard program
        --:    awful.key({ modkey,           }, "Return", function () awful.spawn(terminal) end, {description = "open a terminal", group = "launcher"}),
     awful.key({ modkey, "Control" }, "r",      awesome.restart, {description = "reload awesome", group = "awesome"}),
-    awful.key({ modkey, "Shift"}, "q", awesome.quit, {description = "quit awesome", group = "awesome"}),
+    -- just use CtrAltBksp to quit
 
     awful.key({ modkey,           }, "l",     function () awful.tag.incmwfact( 0.05) end, {description = "increase master width factor", group = "layout"}),
     awful.key({ modkey,           }, "h",     function () awful.tag.incmwfact(-0.05) end,  {description = "decrease master width factor", group = "layout"}),
